b607f68e246064a44d9e33649956a97171e6c0db
Douglas Santos <douglas_motasts@outlook.com>
2023-01-02 12:03:16 -0300

    feature(findRelation.ts): adicionando funcao para pegar os dados do curso junto com os dados dos professores

---
+++ src/teachers/findRelation.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const main = async () => {
+    const result =  await prisma.courses.findMany({
+        include: {
+            teacher : true
+        }
+    })
+
+    console.log(result)
+}
+
+main()
